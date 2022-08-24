02fd600d752bc7e99a893a26a4ab14f85d135aab
Karan Meghe <karanmeghe87@gmail.com>
2022-08-25 03:56:13 +0530

    navbar complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    colors: {
+      "gray-dark": "#f6f6f6",
+      "gray-dark-2": "#f5f7fa",
+      "gray0dark-3": "#f5f7fa",
+
+      "body-clr": "#f0fbff",
+
+      "primary-blue": "#00baf2",
+      "secondary-blue": "#0f4a8a",
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Lato", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
